1c8af69724ab638470bddaebe857e7ed4789bb47
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-08 01:53:34 +0200

    feat(core):wow changes

--- libs/react/src/lib/react.tsx
+++ libs/react/src/lib/react.tsx
@@ -3,6 +3,6 @@ type MyComponentProps = {
 };
 
 export const MyComponent = ({ children }: MyComponentProps) => {
-  console.log('MyComponent is callred');
+  console.log('MyComponent is called');
   return <div>{children}</div>;
 };
