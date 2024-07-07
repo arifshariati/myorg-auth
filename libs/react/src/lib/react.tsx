da9158a3fe891e4360f33675aa6eaa469f27ed17
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-08 01:56:49 +0200

    feat(core): this is feat testing

--- libs/react/src/lib/react.tsx
+++ libs/react/src/lib/react.tsx
@@ -3,6 +3,6 @@ type MyComponentProps = {
 };
 
 export const MyComponent = ({ children }: MyComponentProps) => {
-  console.log('MyComponent is called');
+  console.log('MyComponent is called and 🔴');
   return <div>{children}</div>;
 };
