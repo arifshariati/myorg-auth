c4abc063676cfa2e7861164de68e571f564569ea
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-08 02:15:39 +0200

    feat(core): CI updated-few chanegs

--- libs/react/src/lib/react.tsx
+++ libs/react/src/lib/react.tsx
@@ -3,6 +3,6 @@ type MyComponentProps = {
 };
 
 export const MyComponent = ({ children }: MyComponentProps) => {
-  console.log('MyComponent is called and 🔴');
+  console.log('MyComponent is called and 🔥');
   return <div>{children}</div>;
 };
