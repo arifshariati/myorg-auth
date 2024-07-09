1784e9b7f444b665cd2efb19cf35058183c565e7
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-09 16:18:49 +0200

    fix(react): minor update

--- libs/react/src/lib/react.tsx
+++ libs/react/src/lib/react.tsx
@@ -3,6 +3,6 @@ type MyComponentProps = {
 };
 
 export const MyComponent = ({ children }: MyComponentProps) => {
-  console.log('MyComponent is called and 🔥🔥🔥🚀🚀✅✅🔴');
+  console.log('MyComponent is called and 🔥🔥🔥🚀🚀✅✅🔴🚀');
   return <div>{children}</div>;
 };
