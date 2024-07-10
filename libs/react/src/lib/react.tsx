16ed17a499ece02b8ae4aa0e90bd3762eba4ed5e
Arif Shariati <mohammad.arif.fast@hotmail.com>
2024-07-10 12:25:03 +0200

    fix(react): removed addition icons in react

--- libs/react/src/lib/react.tsx
+++ libs/react/src/lib/react.tsx
@@ -3,6 +3,6 @@ type MyComponentProps = {
 };
 
 export const MyComponent = ({ children }: MyComponentProps) => {
-  console.log('MyComponent is called and 🔥🔥🔥🚀🚀✅✅🔴🚀🚀');
+  console.log('MyComponent is called and 🔥');
   return <div>{children}</div>;
 };
